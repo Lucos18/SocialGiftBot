ad30bc66ab8fe3d2781bbc227888cc3a7d0c961b
Buonpane Luca <lucabuonpane3@gmail.com>
2024-05-16 23:24:25 +0200

    Added logic to complete LIKE, FOLLOW and YOUTUBE videos
    Added multiple new instagram and telegram api calls
    Added more settings
    Implemented case where all campaigns are completed

--- ApiTelethonRequests/telethonUtils.py
+++ ApiTelethonRequests/telethonUtils.py
@@ -1,5 +1,17 @@
 from telethon import TelegramClient
 
+clientTelegram: TelegramClient = None
+
 
 def requestLoginAttemptTelegram(username, apiID, apiHash):
-    return TelegramClient(username, apiID, apiHash)
+    global clientTelegram
+    clientTelegram = TelegramClient(username, apiID, apiHash)
+    return clientTelegram
+
+
+async def sendEarnMoneyMessageChoice():
+    await clientTelegram.send_message(entity='@socialgiftbot', message='🤑 GUADAGNA 🤑')
+
+
+async def sendUnlockSocialCampaignsChoice():
+    await clientTelegram.send_message(entity='@socialgiftbot', message='🔒 SBLOCCA CAMPAGNE SOCIAL 🔒')
--- main.py
+++ main.py
@@ -7,33 +7,37 @@ import time
 from time import sleep
 
 from telethon import TelegramClient, events
-from telethon.tl.types import MessageEntityUrl
+from telethon.tl.types import MessageEntityUrl, MessageEntityTextUrl
 
 import campaignEnum
-from ApiTelethonRequests.telethonUtils import requestLoginAttemptTelegram
+from ApiInstagrapiRequests.instagrapiUtils import likeInstagramPost, requestLoginAttemptInstagram, \
+    followInstagramProfile
+from ApiTelethonRequests.telethonUtils import requestLoginAttemptTelegram, sendEarnMoneyMessageChoice
 
 # PYPPETEER MUST BE ABOVE FROM IMPORT #
 PYPPETEER_CHROMIUM_REVISION = '1263111'
 
 os.environ['PYPPETEER_CHROMIUM_REVISION'] = PYPPETEER_CHROMIUM_REVISION
 
-from pyppeteerClient.pyppeteerUtils import goToPageAndClick, pyppeteerBrowser
+from pyppeteerClient.pyppeteerUtils import goToPageAndClick, pyppeteerBrowser, goToRulesPageAndCompileInformations, \
+    closePyppeteerBrowser
 
 telegramApiCredentialsPath = "telegramApiCredentials.json"
+clientTelegram: TelegramClient = None
+accountTelegramInfo = {}
+
 instagramApiCredentialsPath = "instagramApiCredentials.json"
+accountInstagramInfo = {}
+
+socialGiftInfoPath = "socialGiftInfo.json"
+accountSocialGiftInfo = {}
+
 settingsPath = "settings.json"
+settings = {}
 
 campaignEnumObject = campaignEnum.Campaign
 
-clientTelegram: TelegramClient = None
-
 sleeptime = random.uniform(2, 4)
-
-# TODO need Instagram Client
-
-accountInstagramInfo = {}
-accountTelegramInfo = {}
-settings = {}
 
 
 # Api Information region #
@@ -109,6 +113,41 @@ def instagramApiCredentialsFileWrite():
         return False
 
 
+def socialGiftInfoFileRead():
+    global accountSocialGiftInfo
+    try:
+        assert os.path.isfile(socialGiftInfoPath)
+        with open(socialGiftInfoPath, "r") as apiInfo:
+            accountData = json.load(apiInfo)
+            apiInfo.close()
+            accountSocialGiftInfo = accountData
+            print("Account info for social gift loaded correctly!")
+            return True
+    except Exception as e:
+        print("Error! Account info for social gift not found, file is not found, a new one will be created now.")
+        return False
+
+
+def socialGiftInfoFileWrite():
+    global accountSocialGiftInfo
+    try:
+        accountData = {
+            'personalName': input('Enter your name (used for rules auto-compilation if not already done): '),
+            'personalSurname': input('Enter your surname (used for rules auto-compilation if not already done): '),
+            'personalEmail': input('Enter your email (used for rules auto-compilation if not already done): '),
+        }
+        with open(socialGiftInfoPath, "w") as apiInfo:
+            json.dump(accountData, apiInfo, indent=4)
+            apiInfo.close()
+            accountSocialGiftInfo = accountData
+            print("Account info for social gift saved correctly!")
+            return True
+    except Exception as e:
+        print("Error! Account info for social gift not found, could not create account data file.")
+        print("Code error: " + str(e))
+        return False
+
+
 # Api Information END region #
 
 # Settings region #
@@ -132,6 +171,10 @@ def settingsFileWrite():
         settings = {
             'pauseTimeBetweenEachChallenge': int(input('Enter pause time (seconds) before starting a new challenge: ')),
             'retryCampaign': input('Enter amount of tries before skipping a campaign: '),
+            'autocompleteRulesScenario': str(
+                input('Do you want to auto-complete Rules request? (y/n): ')).lower().strip() == 'y',
+            'debugMode': str(
+                input('Do you want to auto debug mode? (y/n): ')).lower().strip() == 'y',
         }
         with open(settingsPath, "w") as settingsInfo:
             json.dump(settings, settingsInfo, indent=4)
@@ -153,18 +196,28 @@ def waitTimeBeforeContinue(totalTimeToWait):
         sys.stderr.write('\r%4d' % i)
 
 
-def scrapeLinkFromTelegramMessage(event):
+def scrapeLinkFromInnerTextTelegramMessage(event):
     msg = event.message
     for _, inner_text in msg.get_entities_text(MessageEntityUrl):
+        print("Link found in message: ", inner_text)
         url = inner_text
         return url
 
 
+def scrapeLinkFromUrlEntityTelegramMessage(event):
+    msg = event.message
+    for url_entity in msg.get_entities_text(MessageEntityTextUrl):
+        print("Link found in message: ", url_entity)
+        url = url_entity
+        return url
+
+
 async def evaluateAndCompleteCampaign(message, filteredUrl):
+    # todo create better architecture
     campaignCompleted = False
     retryCount = 0
     if message.find(campaignEnumObject.IGTV.value['textMessageToEvaluate']) != -1:
-        while not campaignCompleted:
+        while not campaignCompleted:  # TODO missing retrycount check
             print("Campaign evaluated: IGTV")
             print("Opening website and clicking button...")
             didClickHappenSuccessfully = await goToPageAndClick(filteredUrl)
@@ -172,12 +225,57 @@ async def evaluateAndCompleteCampaign(message, filteredUrl):
                 print("Button click worked successfully!")
                 campaignCompleted = True
                 print("Waiting before confirming the campaign...")
-                waitTimeBeforeContinue(45)
+                waitTimeBeforeContinue(
+                    campaignEnumObject.IGTV.value['timeToWaitBeforeConfirmingCampaign'])  # Correct time is 15 seconds
                 print("Campaign completed")
             else:
                 retryCount = retryCount + 1
                 sleep(sleeptime)
                 print("Error occurred with pyppeteer browser while clicking button, retry number: ", retryCount)
+    if message.find(campaignEnumObject.RULES.value['textMessageToEvaluate']) != -1:
+        # TODO missing RULES case scenario
+        if settings['autocompleteRulesScenario']:
+            await goToRulesPageAndCompileInformations(
+                "https://socialgift.alexdev.it/regole/?id=192481247",  # TODO ADD HERE CORRECT LINK OBTAINING
+                accountSocialGiftInfo['personalName'],
+                accountSocialGiftInfo['personalSurname'],
+                accountSocialGiftInfo['personalEmail']
+            )
+    if message.find(campaignEnumObject.YOUTUBE.value['textMessageToEvaluate']) != -1:
+        waitTimeBeforeContinue(campaignEnumObject.YOUTUBE.value['timeToWaitBeforeConfirmingCampaign'])
+        campaignCompleted = True
+    if message.find(campaignEnumObject.LIKE.value['textMessageToEvaluate']) != -1:
+        resultCampaign = await likeInstagramPost(filteredUrl)
+        if resultCampaign:
+            print('Like campaign completed successfully! waiting some time before continuing...')
+            waitTimeBeforeContinue(
+                campaignEnumObject.LIKE.value['timeToWaitBeforeConfirmingCampaign'] + int(random.uniform(3, 5)))
+            campaignCompleted = True
+        else:
+            print('Like campaign not completed, waiting some time before continuing and skip...')
+            waitTimeBeforeContinue(
+                campaignEnumObject.LIKE.value['timeToWaitBeforeConfirmingCampaign'] + int(random.uniform(1, 2)))
+            campaignCompleted = False
+    if message.find(campaignEnumObject.FOLLOW.value['textMessageToEvaluate']) != -1:
+        resultCampaign = await followInstagramProfile(filteredUrl)
+        if resultCampaign:
+            print('Follow campaign completed successfully! waiting some time before continuing...')
+            waitTimeBeforeContinue(
+                campaignEnumObject.FOLLOW.value['timeToWaitBeforeConfirmingCampaign'] + int(random.uniform(5, 8)))
+            campaignCompleted = True
+        else:
+            print('Follow campaign not completed, waiting some time before continuing and skip...')
+            waitTimeBeforeContinue(
+                campaignEnumObject.FOLLOW.value['timeToWaitBeforeConfirmingCampaign'] + int(random.uniform(60, 120)))
+            campaignCompleted = False
+    if message.find(campaignEnumObject.COMPLETED.value['textMessageToEvaluate']) != -1:
+        print("All campaigns completed! Closing the script...")
+        await closePyppeteerBrowser()
+        exit()
+    if message.find(campaignEnumObject.STORIES.value['textMessageToEvaluate']) != -1:
+        # todo implement stories
+        print("STORIES NOT IMPLEMENTED")
+        exit()
     else:
         print("No campaign have been found from message")
     return campaignCompleted
@@ -203,10 +301,15 @@ if not settingsFileRead():
         print('Could not read or create new settings file, please check Github and follow the steps')
         exit(0)
 
-#clientInstagram = requestLoginAttemptInstagram(
-    #accountInstagramInfo["accountName"],
-    #accountInstagramInfo["accountPassword"]
-#)
+if not socialGiftInfoFileRead():
+    if not socialGiftInfoFileWrite():
+        print('Could not read or create new social gift info file, please check Github and follow the steps')
+        exit(0)
+
+clientInstagram = requestLoginAttemptInstagram(
+    accountInstagramInfo["accountName"],
+    accountInstagramInfo["accountPassword"]
+)
 
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
@@ -220,20 +323,28 @@ with requestLoginAttemptTelegram(
     client = clientTelegram
     client.connect()
 
+    loop.run_until_complete(sendEarnMoneyMessageChoice())
+
 
     @client.on(events.NewMessage(chats='@socialgiftbot', incoming=True, outgoing=False))
     async def handler(event):
         eventRawText = event.raw_text
 
         print("Incoming message from @socialgiftbot")
-        print("message received: ", event.message)
+        # print("message received: ", event.message)
         print("raw message received: ", event.raw_text)
-        filteredUrl = scrapeLinkFromTelegramMessage(event)
+        filteredUrl = scrapeLinkFromInnerTextTelegramMessage(event)
         campaignResult = await evaluateAndCompleteCampaign(eventRawText, filteredUrl)
         if campaignResult:
+            if settings['debugMode']:
+                print('Debug mode: waiting before starting a new campaign. Last campaign return: ' + str(campaignResult))
+                waitTimeBeforeContinue(5)
             await event.message.click(1)
             print("Confirm button clicked! Campaign is completed")
         else:
+            if settings['debugMode']:
+                print('Debug mode: waiting before starting a new campaign. Last campaign return: ' + str(campaignResult))
+                waitTimeBeforeContinue(5)
             await event.message.click(0)
             print("Skip button clicked! Campaign is NOT completed")
 
